62de527c5208cd7c361b36c6b3570f843808bc4b
Hrithik Debnath <debnathhrithikhd7@gmail.com>
2024-03-29 23:32:11 +0600

    Color coded Graph bars added

--- src/components/Read/Read.jsx
+++ src/components/Read/Read.jsx
@@ -2,17 +2,27 @@ import { useState, useEffect } from "react";
 import { getStoredReadBooks } from "../Utility/localstorage";
 import { useLoaderData } from "react-router-dom";
 
-import { BarChart, Bar, XAxis, YAxis, ResponsiveContainer } from "recharts";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  ResponsiveContainer,
+  Cell,
+  CartesianGrid,
+} from "recharts";
 
-const getPath = (x, y, width, height) =>
-  `M${x},${y + height}
-   C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3} ${
-    x + width / 2
-  }, ${y}
-   C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${
+const getPath = (x, y, width, height) => {
+  return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${
+    y + height / 3
+  }
+  ${x + width / 2}, ${y}
+  C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${
     x + width
   }, ${y + height}
-   Z`;
+  Z`;
+};
+const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
 
 const TriangleBar = (props) => {
   const { fill, x, y, width, height } = props;
@@ -43,15 +53,30 @@ const Read = () => {
       <h1 className="text-4xl font-bold">Total Pages to Read</h1>
       <div style={{ width: "100%", height: 500 }}>
         <ResponsiveContainer>
-          <BarChart data={chartData}>
-            <XAxis className="" dataKey="bookName" />
+          <BarChart
+            width={500}
+            height={300}
+            data={chartData}
+            margin={{
+              top: 20,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="bookName" />
             <YAxis />
             <Bar
-              className="font-bold text-2xl"
               dataKey="totalPages"
-              fill="orange"
+              fill="#8884d8"
               shape={<TriangleBar />}
-            />
+              label={{ position: "top" }}
+            >
+              {chartData.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+              ))}
+            </Bar>
           </BarChart>
         </ResponsiveContainer>
       </div>
